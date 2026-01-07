5b53a20fbbd499a894d606a62af0fe8f781b7d2d
SmallShorty <46626284+SmallShorty@users.noreply.github.com>
2026-01-07 18:55:32 +0300

    added forlder templates for html, created view for

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from catalog.models.item import Item
 
-# Create your views here.
+def item_detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    return render(request, 'catalog/item_detail.html', {'item': item})
