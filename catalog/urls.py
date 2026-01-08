40f4fe0ad622c75168f7e067c53b5272108159a0
SmallShorty <46626284+SmallShorty@users.noreply.github.com>
2026-01-08 03:09:04 +0300

    added view add_to_cart and implemented in html template

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('item/<int:pk>/', views.item_detail, name='item_detail'),
+    path('add-to-order/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from catalog.models.item import Item
+from catalog.services.order import OrderService
 
 def item_detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     return render(request, 'catalog/item_detail.html', {'item': item})
+
+def add_to_cart(request, item_id):
+    if request.method == 'POST':
+        order_service = OrderService(request)
+        order_service.add_item(item_id=item_id, quantity=1)
+    
+    return redirect('item_detail', pk=item_id)
